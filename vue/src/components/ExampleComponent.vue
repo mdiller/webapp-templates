f7b43a68be6b0f329bd9c3eb7ebb7fbeb311108c
Malcolm Diller <malcolm@dillerm.io>
2022-08-17 20:22:12 -0700

    updated to be a better template

---
+++ vue/src/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+	msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+	<h1>{{ msg }}</h1>
+	<p>Heres some example text</p>
+</template>
+
+<style scoped>
+
+</style>
